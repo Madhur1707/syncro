d647588341e81f43d28940cc1e7c1c405e90a78f
Madhur1707 <madhurpathak000@gmail.com>
2024-11-18 13:44:09 +0530

    saving user in db and create routes and a custom page for not found

---
+++ app/not-found.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="text-4xl font-semibold w-screen pt-60 grid place-items-center">
+            <h1>404 - Page Not Found</h1>
+        </div>
+    );
+}
